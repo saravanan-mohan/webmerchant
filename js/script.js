c7850637f48e38fa3246de74d3123e2f5c7ceb58
Saravanan Mohan <msrvnm@gmail.com>
2020-02-18 13:04:41 +0530

    adding carousel

--- js/script.js
+++ js/script.js
@@ -1,6 +1,23 @@
 var navEl = document.getElementById('navContainer');
+var carouselEl = document.getElementsByClassName('carousel-item')
 function expand() {
     var expandEl = document.getElementById('expandBtn');
     expandEl.classList.toggle('expanded');
     navEl.classList.toggle('show');
+}
+
+function moveleft(n) {
+    carouselEl[n].classList.remove('show');
+    if(n <= 0) {
+        n = carouselEl.length
+    }
+    carouselEl[n-1].classList.add('show');
+}
+
+function moveright(n) {
+    carouselEl[n].classList.remove('show');
+    if(n >= carouselEl.length-1) {
+        n = -1
+    }
+    carouselEl[n+1].classList.add('show');
 }
